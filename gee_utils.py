bf832316f203190307fe26ba5e59bfbe9f8326dc
Agil <agilfahrezy60@gmail.com>
2025-08-06 10:30:07 +0700

    add a new function for extract the pixel value based on the training data given

--- gee_utils.py
+++ gee_utils.py
@@ -1,4 +1,5 @@
 import ee
+
 def get_sat_embedding(aoi, start_year, end_year):
      """
     Retrieves the satellite embedding data
@@ -20,3 +21,37 @@ def get_sat_embedding(aoi, start_year, end_year):
          .median()\
         .clip(aoi)
      return image
+
+def extract_pixel_value(image, roi, class_property, scale=10, split_ratio = 0.5, tile_scale = 16):
+     """
+     extract the pixels value for supervised classification based on pre defined regions of interest (ROI) and
+     partitioned the data into training and testing
+     Parameters:
+        image (ee.Image)
+        Region of interest/ROI (feature collection)
+        class_property/class_label
+        scale(int), based on spatial resolution of the data
+        split_ratio (float)
+        tile_scale
+     Returns:
+        tuple: (training_samples, testing_samples)
+     """
+     #create a random column
+     roi_random = roi.randomColumn()
+     #partioned the original training data
+     training = roi_random.filter(ee.Filter.lt('random', split_ratio))
+     testing = roi_random.filter(ee.Filter.gte('random', split_ratio))
+     #extract the pixel values
+     training_pixels = image.sampleRegions(
+                        collection=training,
+                        properties = [class_property],
+                        scale = scale,
+                        tileScale = tile_scale 
+     )
+     testing_pixels = image.sampleRegions(
+                        collection=testing,
+                        properties = [class_property],
+                        scale = scale,
+                        tileScale = tile_scale 
+     )
+     return training_pixels, testing_pixels
